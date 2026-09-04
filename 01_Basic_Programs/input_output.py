61d8aeaf4e224f0b99870bc26469e88249aba461
OeishaniPurkayastha <oeishanipurkayastha@gmail.com>
2026-09-05 01:38:19 +0530

    Add a program to take input and display output

---
+++ 01_Basic_Programs/input_output.py
@@ -0,0 +1,9 @@
+# Input and Output in Python
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+temperature = float(input("Enter temperature: "))
+
+print("Temperature:", temperature)
+print("You are", age, "years old.")
+print("Hello,", name)
