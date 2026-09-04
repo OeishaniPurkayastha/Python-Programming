5f8aee48f0975afe20ac717bb9ccb56ec4f5f103
OeishaniPurkayastha <oeishanipurkayastha@gmail.com>
2026-09-05 01:00:14 +0530

    Add a program to display data types

---
+++ 01_Basic_Programs/data_types.py
@@ -0,0 +1,9 @@
+name = "Oeishani"      # string
+age = 19               # integer
+height = 5.4           # float
+is_student = True      # boolean
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
